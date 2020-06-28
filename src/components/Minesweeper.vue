100278dd13efb76f3e94e9a636098d1bb418ad70
Jozef Plata <plata.jozef@example.com>
2020-06-29 04:31:53 +0930

    Init Commit. Got columns and rows displayed

---
+++ src/components/Minesweeper.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="minefield">
+        <li class="column" v-for="col in columns" :key="col">
+            <ul class="row" v-for="row in rows" :key="row">
+                <Field />
+            </ul>
+        </li>
+    </div>
+</template>
+
+<script>
+    import Field from "@/components/Field";
+
+    export default {
+        name: "Minesweeper",
+        components: {
+            Field,
+        },
+        data() {
+            return {
+                name: this.name,
+                rows: 7,
+                columns: 7,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    .minefield {
+        display: flex;
+        justify-content: center;
+    }
+
+    .column {
+        display: inline-block;
+        margin: 0;
+    }
+</style>
