5fc61f535548594654b5c7dfa71d196d0f42613e
Jozef Plata <plata.jozef@example.com>
2020-06-29 10:59:04 +0930

    Added colouring with scss. No need for involving objects for now.
    Next: Add options to unreveal only @click

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="square" @click="selectField">
+    <div :class="classObject" @click="selectField">
         {{ field }}
     </div>
 </template>
@@ -18,12 +18,39 @@
                 default: 'X',
             },
         },
+        computed: {
+            classObject: function () {
+                if (this.field === 'X')
+                    return 'square unexplored';
+                else if (this.field === '0')
+                    return 'square v0';
+                else if (this.field === '1')
+                    return 'square v1';
+                else if (this.field === '2')
+                    return 'square v2';
+                else if (this.field === '3')
+                    return 'square v3';
+                else if (this.field === '4')
+                    return 'square v4';
+                else if (this.field === '5')
+                    return 'square v5';
+                else if (this.field === '6')
+                    return 'square v6';
+                else if (this.field === '7')
+                    return 'square v7';
+                else if (this.field === '8')
+                    return 'square v8';
+                else
+                    return 'square';
+
+            }
+        },
     }
 </script>
 
 <style scoped>
     .square {
-        background: #fff;
+        background: #c9c9c9;
         border: 1px solid #999;
         float: left;
         font-size: 24px;
@@ -35,5 +62,36 @@
         padding: 0;
         text-align: center;
         width: 34px;
+    }
+    .unexplored {
+        background: #585858;
+        color: rgba(255, 255, 255, 0);
+    }
+    .v0 {
+        color: rgba(255, 255, 255, 0);
+    }
+    .v1 {
+        color: #f8ff74;
+    }
+    .v2 {
+        color: #74f7ff;
+    }
+    .v3 {
+        color: #8a82ff;
+    }
+    .v4 {
+        color: #3046ff;
+    }
+    .v5 {
+        color: #15fd2e;
+    }
+    .v6 {
+        color: #eefc07;
+    }
+    .v7 {
+        color: #ff571a;
+    }
+    .v8 {
+        color: #ff13af;
     }
 </style>
